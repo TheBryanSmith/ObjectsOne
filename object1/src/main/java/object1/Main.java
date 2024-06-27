98a082d2e695296bcf6dca3385fb5284e4187e4c
Bryan <bryan@Zipcoders-MacBook-Pro-5.local>
2024-06-27 14:18:57 -0400

    Finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,28 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+        Person person = new Person("Hillary", 40, 72);
+
+        Person[] people = new Person[10]; // an array of 10 Person objects
+
+        Person person1 = new Person("Alice", 25, 50);
+        Person person2 = new Person("Bob", 30,65);
+
+        people[0] = person1;
+        people[1] = person2;
+
+        System.out.println(person1);
+
+        System.out.println(person);
+
+        System.out.println(person2);
+
+// what is an array? see below...
+
+
+
+
+    }
 }
